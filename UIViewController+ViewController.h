8c34109795b1e6500ae37e0d9fcdc8ec753e7e35
Bo Guthrie & Nick Siefken & Peter Macaluso <pair+boguth.nsiefken.pmacaluso3@devbootcamp.com>
2015-07-21 13:59:24 -0500

    Comitting contact fixes.

---
+++ UIViewController+ViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+ViewController.h
+//  Bump
+//
+//  Created by Apprentice on 7/21/15.
+//  Copyright (c) 2015 Bump Boys!, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (ViewController)
+
+@end
